6085931c843e7adf4530b535fbd29710c40250d0
Anurag Patil <49953149+Anu1996rag@users.noreply.github.com>
2020-05-13 11:11:54 +0530

    Add files via upload

---
+++ Sentiment_Analysis_using_Flask/app.py
@@ -0,0 +1,38 @@
+from flask import Flask,request,render_template,url_for
+from flasgger import Swagger
+
+# Import SentimentIntensityAnalyzer And Flask 
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+#creating object for the SentimentIntensityAnalyzer Class
+sid = SentimentIntensityAnalyzer()
+
+#Declaring the name 
+app = Flask(__name__)
+
+
+#Main Program
+@app.route('/',methods=["POST","GET"])
+def predict():
+    if request.method=='POST':
+        string = request.form['sentence']
+        
+        if string == 'None' or string =="": #this statement is to handle null values
+            return render_template('index.html',sentence = "Please enter a sentence")
+        else:
+            scores = sid.polarity_scores(string)
+        
+            if scores['compound'] == 0:
+                sentiment = "Neutral"
+            elif scores['compound'] > 0:
+                sentiment = "Positive"
+            else:
+                sentiment = "Negative"
+        return render_template('index.html',sentiment = sentiment,sentence = string)
+    else:
+    	result = "method invalid passed"
+    	return render_template('index.html',result = result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
